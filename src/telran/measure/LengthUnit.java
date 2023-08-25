cae7a437cff178533c3454dc5c68e4f23b87169c
RomanSavushkin <romarsa82@gmail.com>
2023-08-25 18:59:16 +0300

    1

--- src/telran/measure/LengthUnit.java
+++ src/telran/measure/LengthUnit.java
@@ -10,7 +10,7 @@ public enum LengthUnit {
 	}
 
 	public Length between(Length l1, Length l2) {
-		// TODO
+		
 		// returns Length object as length between l1 and l2 in "this" units
 		// Example: LengthUnit.M.between (new Length(200, LengthUnit.CM), new Length(1,
 		// LengthUnit.M))
